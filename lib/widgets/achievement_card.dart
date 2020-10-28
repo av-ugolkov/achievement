a141a7de607ac0cd2f62d91c1cab11ef3468e968
makedonskiy <makedonskiy07@gmail.com>
2020-10-28 23:21:11 +0300

    AchievementCard

---
+++ lib/widgets/achievement_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class AchievementCard extends StatelessWidget {
+  final bool _isHaveImage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 75,
+      padding: const EdgeInsets.all(5.0),
+      decoration: BoxDecoration(),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: _isHaveImage
+                ? Center(child: Image.asset(''))
+                : Center(child: Icon(Icons.not_interested)),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name Achievement',
+                          maxLines: 1,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Description',
+                          maxLines: 2,
+                          softWrap: true,
+                          style: TextStyle(fontSize: 12, color: Colors.black45),
+                        ),
+                      ],
+                    ),
+                    Flex(
+                        direction: Axis.horizontal,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            'Create Date',
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.black45),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            'Finish Date',
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.black45),
+                          ),
+                        ]),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
