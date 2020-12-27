97014b3d3b814b95e7e7fcd158f51aed49b3ce5c
makedonskiy <makedonskiy07@gmail.com>
2020-12-27 21:51:08 +0300

    добавление новых форматов локализации даты и времени

--- lib/utils/formate_date.dart
+++ lib/utils/formate_date.dart
@@ -9,7 +9,11 @@ class FormateDate {
           DateFormat(DateFormat.YEAR_MONTH_DAY, _currentLocale),
       DateFormat.YEAR_NUM_MONTH_DAY:
           DateFormat(DateFormat.YEAR_NUM_MONTH_DAY, _currentLocale),
-      DateFormat.WEEKDAY: DateFormat(DateFormat.WEEKDAY, _currentLocale)
+      DateFormat.WEEKDAY: DateFormat(DateFormat.WEEKDAY, _currentLocale),
+      DateFormat.HOUR24_MINUTE_SECOND:
+          DateFormat(DateFormat.HOUR24_MINUTE_SECOND, _currentLocale),
+      DateFormat.HOUR24_MINUTE:
+          DateFormat(DateFormat.HOUR24_MINUTE, _currentLocale)
     };
   }
 
@@ -20,20 +24,22 @@ class FormateDate {
   Map<String, DateFormat> _dateFormat;
 
   static String yearMonthDay(DateTime dateTime) {
-    if (_inst._dateFormat.containsKey(DateFormat.YEAR_MONTH_DAY))
-      return _inst._dateFormat[DateFormat.YEAR_MONTH_DAY].format(dateTime);
-    return dateTime.toString();
+    return _inst._dateFormat[DateFormat.YEAR_MONTH_DAY].format(dateTime);
   }
 
   static String yearNumMonthDay(DateTime dateTime) {
-    if (_inst._dateFormat.containsKey(DateFormat.YEAR_NUM_MONTH_DAY))
-      return _inst._dateFormat[DateFormat.YEAR_NUM_MONTH_DAY].format(dateTime);
-    return dateTime.toString();
+    return _inst._dateFormat[DateFormat.YEAR_NUM_MONTH_DAY].format(dateTime);
   }
 
   static String weekDayName(DateTime dateTime) {
-    if (_inst._dateFormat.containsKey(DateFormat.WEEKDAY))
-      return _inst._dateFormat[DateFormat.WEEKDAY].format(dateTime);
-    return dateTime.toString();
+    return _inst._dateFormat[DateFormat.WEEKDAY].format(dateTime);
+  }
+
+  static String hour24MinuteSecond(DateTime dateTime) {
+    return _inst._dateFormat[DateFormat.HOUR24_MINUTE_SECOND].format(dateTime);
+  }
+
+  static String hour24Minute(DateTime dateTime) {
+    return _inst._dateFormat[DateFormat.HOUR24_MINUTE].format(dateTime);
   }
 }
