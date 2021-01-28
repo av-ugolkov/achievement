06a2b92edaa76c50262e0322325bf11959e193bf
makedonskiy <makedonskiy07@gmail.com>
2021-01-29 00:06:32 +0300

    удаление лишних методов для уведомлений

--- lib/utils/local_notification.dart
+++ lib/utils/local_notification.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotification {
@@ -24,77 +23,12 @@ class LocalNotification {
         onSelectNotification: selectNotification);
   }
 
-  /*Future _showNotificationWithSound() async {
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        'your channel id', 'your channel name', 'your channel description',
-        sound: 'slow_spring_board',
-        importance: Importance.max,
-        priority: Priority.high);
-    var iOSPlatformChannelSpecifics =
-        new IOSNotificationDetails(sound: "slow_spring_board.aiff");
-    var platformChannelSpecifics = new NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'New Post',
-      'How to Show Notification in Flutter',
-      platformChannelSpecifics,
-      payload: 'Custom_Sound',
-    );
+  static scheduleNotification(DateTime scheduledNotificationDateTime) async {
+    await _inst._scheduleNotification(scheduledNotificationDateTime);
   }
 
-  Future _showNotificationWithDefaultSound() async {
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        '0', 'your channel name', 'your channel description',
-        importance: Importance.max, priority: Priority.high);
-    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-    var platformChannelSpecifics = new NotificationDetails(
-        android: androidPlatformChannelSpecifics,
-        iOS: iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'New Post',
-      'How to Show Notification in Flutter',
-      platformChannelSpecifics,
-      payload: 'Default_Sound',
-    );
-  }
-
-  Future _showNotificationWithoutSound() async {
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        '0', 'your channel name', 'your channel description',
-        playSound: false, importance: Importance.max, priority: Priority.high);
-    var iOSPlatformChannelSpecifics =
-        new IOSNotificationDetails(presentSound: false);
-    var platformChannelSpecifics = new NotificationDetails(
-        android: androidPlatformChannelSpecifics,
-        iOS: iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'New Post',
-      'How to Show Notification in Flutter',
-      platformChannelSpecifics,
-      payload: 'No_Sound',
-    );
-  }*/
-
-  static createNotification() async {
-    await _inst._createNotification();
-  }
-
-  Future<void> _createNotification() async {
-    var android = AndroidNotificationDetails('0', channel, 'description',
-        priority: Priority.high, importance: Importance.max);
-    var iOS = IOSNotificationDetails();
-    var platform = new NotificationDetails(android: android, iOS: iOS);
-    await flutterLocalNotificationsPlugin.show(
-        0, 'Flutter devs', 'Flutter Local Notification Demo', platform,
-        payload: 'Welcome to the Local Notification demo');
-  }
-
-  Future<void> scheduleNotification() async {
-    var scheduledNotificationDateTime =
-        DateTime.now().add(Duration(seconds: 5));
+  Future<void> _scheduleNotification(
+      DateTime scheduledNotificationDateTime) async {
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
       '0',
       channel,
@@ -114,47 +48,15 @@ class LocalNotification {
         platformChannelSpecifics,
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.wallClockTime);
-  }
-
-  Future<void> showBigPictureNotification() async {
-    var bigPictureStyleInformation = BigPictureStyleInformation(
-      DrawableResourceAndroidBitmap("flutter_devs"),
-      largeIcon: DrawableResourceAndroidBitmap("flutter_devs"),
-      contentTitle: 'flutter devs',
-      summaryText: 'summaryText',
-    );
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        '0', channel, 'big text channel description',
-        styleInformation: bigPictureStyleInformation);
-    var platformChannelSpecifics = NotificationDetails(
-        android: androidPlatformChannelSpecifics, iOS: null);
-    await flutterLocalNotificationsPlugin.show(
-        0, 'big text title', 'silent body', platformChannelSpecifics,
-        payload: "big image notifications");
-  }
-
-  Future<void> showNotificationMediaStyle() async {
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      '0',
-      channel,
-      'media channel description',
-      color: Colors.red,
-      enableLights: true,
-      largeIcon: DrawableResourceAndroidBitmap("flutter_devs"),
-      styleInformation: MediaStyleInformation(),
-    );
-    var platformChannelSpecifics = NotificationDetails(
-        android: androidPlatformChannelSpecifics, iOS: null);
-    await flutterLocalNotificationsPlugin.show(
-        0, 'notification title', 'notification body', platformChannelSpecifics);
-  }
-
-  Future<void> cancelAllNotification() async {
-    await flutterLocalNotificationsPlugin.cancelAll();
+            UILocalNotificationDateInterpretation.wallClockTime,
+        matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime);
   }
 
   Future<void> cancelNotification(int id) async {
     await flutterLocalNotificationsPlugin.cancel(id);
+  }
+
+  Future<void> cancelAllNotification() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
   }
 }
