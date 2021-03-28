4042a9dea65f4a5a67ca0a25b61447c93e72e07f
makedonskiy <makedonskiy07@gmail.com>
2021-03-28 22:49:22 +0300

    напоминание обворачиваем в карточку

--- lib/widgets/remind_day_widget.dart
+++ lib/widgets/remind_day_widget.dart
@@ -43,80 +43,86 @@ class _RemindDayState extends State<RemindDay> {
         widget.callbackRemove.call(widget);
       },
       child: Container(
-        height: 42,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            DropdownButton<TypeRepition>(
-              value: _typeRepition,
-              onChanged: (TypeRepition? value) {
-                setState(() {
-                  _typeRepition = value ?? TypeRepition.none;
-                  widget.remindModel.typeRepition = _typeRepition;
-                });
-              },
-              items: TypeRepition.values
-                  .map<DropdownMenuItem<TypeRepition>>((value) {
-                return DropdownMenuItem<TypeRepition>(
-                  value: value,
-                  child: Text(
-                    _getStringRepition(value),
-                    style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              DropdownButton<TypeRepition>(
+                value: _typeRepition,
+                onChanged: (TypeRepition? value) {
+                  setState(() {
+                    _typeRepition = value ?? TypeRepition.none;
+                    widget.remindModel.typeRepition = _typeRepition;
+                  });
+                },
+                items: TypeRepition.values
+                    .map<DropdownMenuItem<TypeRepition>>((value) {
+                  return DropdownMenuItem<TypeRepition>(
+                    value: value,
+                    child: Text(
+                      _getStringRepition(value),
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  );
+                }).toList(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () async {
+                      var newRemindDate = await showDatePicker(
+                          context: context,
+                          initialDate: remindDateTime.dateTime,
+                          firstDate: _dateTimeRange.start,
+                          lastDate: _dateTimeRange.end);
+
+                      if (newRemindDate != null) {
+                        setState(() {
+                          remindDateTime = RemindDateTime.fromDateTime(
+                              dateTime: newRemindDate);
+                          widget.remindModel.remindDateTime = remindDateTime;
+                        });
+                      }
+                    },
+                    child: Text(
+                      FormateDate.yearNumMonthDay(remindDateTime.dateTime),
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                );
-              }).toList(),
-            ),
-            TextButton(
-              onPressed: () async {
-                var newRemindDate = await showDatePicker(
-                    context: context,
-                    initialDate: remindDateTime.dateTime,
-                    firstDate: _dateTimeRange.start,
-                    lastDate: _dateTimeRange.end);
+                  TextButton(
+                    onPressed: () async {
+                      var newTimeOfDay = await showTimePicker(
+                          context: context,
+                          initialTime:
+                              TimeOfDay.fromDateTime(remindDateTime.dateTime));
 
-                if (newRemindDate != null) {
-                  setState(() {
-                    remindDateTime =
-                        RemindDateTime.fromDateTime(dateTime: newRemindDate);
-                    widget.remindModel.remindDateTime = remindDateTime;
-                  });
-                }
-              },
-              child: Text(
-                FormateDate.yearNumMonthDay(remindDateTime.dateTime),
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            TextButton(
-              onPressed: () async {
-                var newTimeOfDay = await showTimePicker(
-                    context: context,
-                    initialTime:
-                        TimeOfDay.fromDateTime(remindDateTime.dateTime));
-
-                if (newTimeOfDay != null) {
-                  setState(() {
-                    remindDateTime.hour = newTimeOfDay.hour;
-                    remindDateTime.minute = newTimeOfDay.minute;
-                    widget.remindModel.remindDateTime = remindDateTime;
-                  });
-                }
-              },
-              child: Text(
-                FormateDate.hour24Minute(remindDateTime.dateTime),
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
+                      if (newTimeOfDay != null) {
+                        setState(() {
+                          remindDateTime.hour = newTimeOfDay.hour;
+                          remindDateTime.minute = newTimeOfDay.minute;
+                          widget.remindModel.remindDateTime = remindDateTime;
+                        });
+                      }
+                    },
+                    child: Text(
+                      FormateDate.hour24Minute(remindDateTime.dateTime),
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
