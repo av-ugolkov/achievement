d9dd94a0150116d83090ac72c228e35be7cd6d27
makedonskiy <makedonskiy07@gmail.com>
2021-04-14 15:04:25 +0300

    начал правки интерфейса показа ачивки

--- lib/pages/view_achievement_page.dart
+++ lib/pages/view_achievement_page.dart
@@ -5,105 +5,158 @@ import 'package:achievement/db/db_remind.dart';
 import 'package:achievement/enums.dart';
 import 'package:achievement/model/achievement_model.dart';
 import 'package:achievement/model/remind_model.dart';
-import 'package:achievement/utils/formate_date.dart';
 import 'package:achievement/widgets/expandable_fab.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_components/components/date_time_progress/date_time_progress.dart';
 
-class ViewAchievementPage extends StatelessWidget {
+class ViewAchievementPage extends StatefulWidget {
+  @override
+  _ViewAchievementPageState createState() => _ViewAchievementPageState();
+}
+
+class _ViewAchievementPageState extends State<ViewAchievementPage> {
+  late AchievementModel _achievementModel;
+  late DateTime _currentDateTime;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _currentDateTime = DateTime.now();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var settings = ModalRoute.of(context)!.settings;
-    var achievementModel = settings.arguments as AchievementModel;
+    var settings = ModalRoute.of(context)?.settings;
+    _achievementModel = settings!.arguments as AchievementModel;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(getLocaleOfContext(context).viewAchievementTitle),
       ),
-      floatingActionButton: ExpandableFab(
-        distance: 112.0,
-        children: [
-          ActionButton(
-            onPressed: () {
-              _setAchievementState(achievementModel, AchievementState.fail);
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.block_outlined),
-            color: Colors.red,
-          ),
-          ActionButton(
-            onPressed: () {
-              _setAchievementState(achievementModel, AchievementState.archived);
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.archive_outlined),
-            color: Colors.grey,
-          ),
-          ActionButton(
-            onPressed: () {
-              _setAchievementState(achievementModel, AchievementState.done);
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.done),
-            color: Colors.green,
-          ),
-          ActionButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/create_achievement_page');
-              _setAchievementState(achievementModel, AchievementState.active);
-            },
-            icon: const Icon(Icons.edit),
-          ),
-        ],
-      ),
+      floatingActionButton: _floatingActionButton(),
       body: ListView(
         padding: EdgeInsets.all(10),
         children: [
-          Container(
-            alignment: Alignment.topLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  achievementModel.header,
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  achievementModel.description,
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            width: 100,
-            height: 100,
-            child: achievementModel.imagePath.isEmpty
-                ? Icon(
-                    Icons.not_interested,
-                    color: Colors.grey[300],
-                    size: 100,
-                  )
-                : Image.file(
-                    File(achievementModel.imagePath),
-                  ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(FormateDate.yearMonthDay(achievementModel.createDate)),
-              Text(FormateDate.yearMonthDay(achievementModel.finishDate))
-            ],
-          ),
-          Checkbox(
-              value: achievementModel.remindIds.isNotEmpty, onChanged: null),
-          Container(
-            child: (achievementModel.remindIds.isEmpty)
-                ? null
-                : _remindWidget(achievementModel.remindIds),
-          )
+          _title(),
+          _description(),
+          _dateTimeProgress(),
+          _reminds(),
         ],
       ),
+    );
+  }
+
+  Widget _floatingActionButton() {
+    return ExpandableFab(
+      distance: 112.0,
+      children: [
+        ActionButton(
+          onPressed: () {
+            _setAchievementState(_achievementModel, AchievementState.fail);
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.block_outlined),
+          color: Colors.red,
+        ),
+        ActionButton(
+          onPressed: () {
+            _setAchievementState(_achievementModel, AchievementState.archived);
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.archive_outlined),
+          color: Colors.grey,
+        ),
+        ActionButton(
+          onPressed: () {
+            _setAchievementState(_achievementModel, AchievementState.done);
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.done),
+          color: Colors.green,
+        ),
+        ActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/create_achievement_page');
+            _setAchievementState(_achievementModel, AchievementState.active);
+          },
+          icon: const Icon(Icons.edit),
+        ),
+      ],
+    );
+  }
+
+  Widget _title() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 75,
+          height: 75,
+          child: _achievementModel.imagePath.isEmpty
+              ? Icon(
+                  Icons.not_interested,
+                  color: Colors.grey[300],
+                  size: 75,
+                )
+              : Image.file(
+                  File(_achievementModel.imagePath),
+                ),
+        ),
+        SizedBox(width: 4),
+        DecoratedBox(
+          decoration: BoxDecoration(
+              border:
+                  Border(bottom: BorderSide(color: Colors.black, width: 2))),
+          child: Text(
+            _achievementModel.header,
+            maxLines: 2,
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _description() {
+    if (_achievementModel.description.isEmpty) {
+      return Container();
+    }
+    return Text(
+      _achievementModel.description,
+      style: TextStyle(fontSize: 18, color: Colors.black54),
+    );
+  }
+
+  Widget _dateTimeProgress() {
+    return Container(
+      child: DateTimeProgress(
+        start: _achievementModel.createDate,
+        finish: _achievementModel.finishDate,
+        current: _currentDateTime,
+        onChanged: (dateTime) {
+          _currentDateTime = dateTime;
+        },
+      ),
+    );
+  }
+
+  Widget _reminds() {
+    if (_achievementModel.remindIds.isEmpty) {
+      return Container();
+    }
+    return Column(
+      children: [
+        Container(
+          child: (_achievementModel.remindIds.isEmpty)
+              ? null
+              : _remindWidget(_achievementModel.remindIds),
+        )
+      ],
     );
   }
 
