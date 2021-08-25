4d5dd1c390919b9f9b49b397153d17d2c4120eb5
makedonskiy <makedonskiy07@gmail.com>
2021-08-25 22:41:32 +0300

    DarkMode

---
+++ lib/ui/settings_page/dark_mode.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DarkMode extends StatefulWidget {
+  const DarkMode({Key? key}) : super(key: key);
+
+  @override
+  _DarkModeState createState() => _DarkModeState();
+}
+
+class _DarkModeState extends State<DarkMode> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
