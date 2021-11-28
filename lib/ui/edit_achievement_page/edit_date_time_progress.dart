a84d2910851205117527092817f2444fbadff584
av-ugolkov <ugolkov.prog@gmail.com>
2021-11-28 11:02:42 +0300

    новый способ выбора даты начала и конца ачивки

--- lib/ui/edit_achievement_page/edit_date_time_progress.dart
+++ lib/ui/edit_achievement_page/edit_date_time_progress.dart
@@ -1,6 +1,8 @@
+import 'package:achievement/bridge/localization.dart';
 import 'package:achievement/core/changed_date_time_range.dart';
 import 'package:achievement/core/formate_date.dart';
 import 'package:achievement/ui/edit_achievement_page/edit_remind_card/form_edit_remind_card.dart';
+import 'package:achievement/ui/edit_achievement_page/number_picker_panel.dart';
 import 'package:flutter/material.dart';
 
 class EditDateTimeProgress extends StatefulWidget {
@@ -23,73 +25,99 @@ class _EditDateTimeProgressState extends State<EditDateTimeProgress> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Старт ачивки'),
-            TextButton(
-              onPressed: () async {
-                FocusScope.of(context).unfocus();
-                var selectDate = await showDatePicker(
-                    context: context,
-                    initialDate: widget.dateRangeAchievement.start,
-                    firstDate: DateTime(0),
-                    lastDate: widget.dateRangeAchievement.end);
-                setState(() {
-                  if (selectDate != null) {
-                    widget.dateRangeAchievement.start = selectDate;
-                    if (_hasRemind) {
-                      widget.remindCards.removeWhere((remind) {
-                        var start = remind.remindModel.remindDateTime.dateTime
-                            .compareTo(widget.dateRangeAchievement.start);
-                        var end = remind.remindModel.remindDateTime.dateTime
-                            .compareTo(widget.dateRangeAchievement.end);
-                        return start < 0 || end > 0;
-                      });
-                    }
-                  }
+        _rowDateTime(getLocaleOfContext(context).start_achiev,
+            widget.dateRangeAchievement.start, () async {
+          FocusScope.of(context).unfocus();
+          var selectDate = await showDatePicker(
+              context: context,
+              initialDate: widget.dateRangeAchievement.start,
+              firstDate: DateTime(0),
+              lastDate: widget.dateRangeAchievement.end);
+          setState(() {
+            if (selectDate != null) {
+              widget.dateRangeAchievement.start = selectDate;
+              if (_hasRemind) {
+                widget.remindCards.removeWhere((remind) {
+                  var start = remind.remindModel.remindDateTime.dateTime
+                      .compareTo(widget.dateRangeAchievement.start);
+                  var end = remind.remindModel.remindDateTime.dateTime
+                      .compareTo(widget.dateRangeAchievement.end);
+                  return start < 0 || end > 0;
                 });
-              },
-              child: Text(
-                FormateDate.yearMonthDay(widget.dateRangeAchievement.start),
-              ),
-            ),
-          ],
+              }
+            }
+          });
+        }),
+        _rowDateTime(
+          getLocaleOfContext(context).finish_achiev,
+          widget.dateRangeAchievement.end,
+          () async {
+            FocusScope.of(context).unfocus();
+            var selectDate = await showDatePicker(
+                context: context,
+                initialDate: widget.dateRangeAchievement.end,
+                firstDate: widget.dateRangeAchievement.start,
+                lastDate: DateTime(9999));
+            setState(() {
+              if (selectDate != null) {
+                widget.dateRangeAchievement.end = selectDate;
+                if (_hasRemind) {
+                  widget.remindCards.removeWhere((remind) {
+                    var start = remind.remindModel.remindDateTime.dateTime
+                        .compareTo(widget.dateRangeAchievement.start);
+                    var end = remind.remindModel.remindDateTime.dateTime
+                        .compareTo(widget.dateRangeAchievement.end);
+                    return start < 0 || end > 0;
+                  });
+                }
+              }
+            });
+          },
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Финиш ачивки'),
-            TextButton(
-              onPressed: () async {
-                FocusScope.of(context).unfocus();
-                var selectDate = await showDatePicker(
-                    context: context,
-                    initialDate: widget.dateRangeAchievement.end,
-                    firstDate: widget.dateRangeAchievement.start,
-                    lastDate: DateTime(9999));
+            Text(
+              getLocaleOfContext(context).duration_achiev,
+            ),
+            NumberPickerPanel(
+              delta: widget.dateRangeAchievement.end
+                  .difference(widget.dateRangeAchievement.start)
+                  .inDays,
+              onEditComplete: (value) {
                 setState(() {
-                  if (selectDate != null) {
-                    widget.dateRangeAchievement.end = selectDate;
-                    if (_hasRemind) {
-                      widget.remindCards.removeWhere((remind) {
-                        var start = remind.remindModel.remindDateTime.dateTime
-                            .compareTo(widget.dateRangeAchievement.start);
-                        var end = remind.remindModel.remindDateTime.dateTime
-                            .compareTo(widget.dateRangeAchievement.end);
-                        return start < 0 || end > 0;
-                      });
-                    }
-                  }
+                  widget.dateRangeAchievement.end = widget
+                      .dateRangeAchievement.start
+                      .add(Duration(days: value));
                 });
               },
-              child: Text(
-                FormateDate.yearMonthDay(widget.dateRangeAchievement.end),
-              ),
-            ),
+            )
           ],
-        )
+        ),
       ],
+    );
+  }
+
+  Widget _rowDateTime(String label, DateTime dateTime, void Function() func) {
+    return SizedBox(
+      height: 36,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+          ),
+          TextButton(
+            onPressed: func,
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.all(0),
+            ),
+            child: Text(
+              FormateDate.yearMonthDay(dateTime),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
