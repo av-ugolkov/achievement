a8729cf0ece00f1c6179ec7105e3fa24ce690a00
av-ugolkov <ugolkov.prog@gmail.com>
2021-11-28 11:24:36 +0300

    локализация

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -8,6 +8,7 @@
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
 // ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -28,16 +29,19 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Create achievement"),
         "description": MessageLookupByLibrary.simpleMessage("Description"),
         "done": MessageLookupByLibrary.simpleMessage("Done"),
+        "duration_achiev": MessageLookupByLibrary.simpleMessage("Duration"),
         "error_header": MessageLookupByLibrary.simpleMessage(
             "The title cannot be less than 3 characters"),
         "error_remind_card":
             MessageLookupByLibrary.simpleMessage("Selected date in the past"),
         "fail": MessageLookupByLibrary.simpleMessage("Fail"),
+        "finish_achiev": MessageLookupByLibrary.simpleMessage("Finish achiev"),
         "finished": MessageLookupByLibrary.simpleMessage("Finished"),
         "header": MessageLookupByLibrary.simpleMessage("Header"),
         "remind": MessageLookupByLibrary.simpleMessage("Remind"),
         "repeat": MessageLookupByLibrary.simpleMessage("Repeat"),
         "settings": MessageLookupByLibrary.simpleMessage("Settings"),
+        "start_achiev": MessageLookupByLibrary.simpleMessage("Start achiev"),
         "view_achievement_title":
             MessageLookupByLibrary.simpleMessage("Achievement"),
         "what_do_you_do":
