32233de3cb1281e786df1a342ff42a1498d040b9
av-ugolkov <ugolkov.prog@gmail.com>
2021-12-04 22:56:11 +0300

    добавление файрбейз аналитики и крашлитик

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,16 @@ import 'package:achievement/ui/achievements_page/achievements_page.dart';
 import 'package:achievement/ui/edit_achievement_page/edit_achievement_page.dart';
 import 'package:achievement/ui/settings_page/settings_page.dart';
 import 'package:achievement/ui/view_achievement_page/view_achievement_page.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:achievement/generated/l10n.dart';
 import 'package:achievement/core/utils.dart' as utils;
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +25,16 @@ void main() {
     var docsDir = await getApplicationDocumentsDirectory();
     utils.docsDir = docsDir;
 
+    await Firebase.initializeApp(
+      name: 'com.ugolkov.achievement',
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBzrHcu4TQK8Ji31Bnyr8faDQwEanONPDE',
+        appId: '1:1087083017957:android:160daff30995acc3d1460c',
+        messagingSenderId: '1087083017957',
+        projectId: 'achievement-dc79f',
+      ),
+    );
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
     runApp(MyApp());
   }
 
@@ -30,6 +45,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,6 +68,7 @@ class MyApp extends StatelessWidget {
         RouteSettingsPage: (context) => SettingsPage(),
         RouteAboutPage: (context) => AboutPage()
       },
+      navigatorObservers: <NavigatorObserver>[observer],
       theme: buildThemeLight(),
       darkTheme: buildThemeDark(),
       home: AchievementsPage(),
